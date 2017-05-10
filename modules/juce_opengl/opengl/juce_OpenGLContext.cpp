fc3fc1eeb6d1488c1d0c3b893aac9d80c6ec0247
Jesse Chappell <jesse@essej.net>
2017-05-10 13:17:12 -0400

    hack to provide optional fix for insane opengl bug on some android devices like the HTC One

--- modules/juce_opengl/opengl/juce_OpenGLContext.cpp
+++ modules/juce_opengl/opengl/juce_OpenGLContext.cpp
@@ -922,6 +922,16 @@ void OpenGLContext::setOpenGLVersionRequired (OpenGLVersion v) noexcept
     versionRequired = v;
 }
 
+void OpenGLContext::setMobileBufferBugMitigation(bool flag)
+{
+    mobileBufferBugMitigation = flag;
+}
+
+bool OpenGLContext::getMobileBufferBugMitigation() const
+{
+    return mobileBufferBugMitigation;
+}
+
 void OpenGLContext::attachTo (Component& component)
 {
     component.repaint();
