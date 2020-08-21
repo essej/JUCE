d4fcedd525440cfcde1c092448f3ecc7f87a1294
Jesse Chappell <jesse@sonosaurus.com>
2020-08-21 13:57:29 -0400

    removed assert in iskioskmodeview

--- modules/juce_gui_basics/native/juce_ios_UIViewComponentPeer.mm
+++ modules/juce_gui_basics/native/juce_ios_UIViewComponentPeer.mm
@@ -339,9 +339,12 @@ static void sendScreenBoundsUpdate (JuceUIViewController* c)
 static bool isKioskModeView (JuceUIViewController* c)
 {
     JuceUIView* juceView = (JuceUIView*) [c view];
-    jassert (juceView != nil && juceView->owner != nullptr);
+    //jassert (juceView != nil && juceView->owner != nullptr);
 
-    return Desktop::getInstance().getKioskModeComponent() == &(juceView->owner->getComponent());
+    if (juceView != nil && juceView->owner != nullptr) {
+        return Desktop::getInstance().getKioskModeComponent() == &(juceView->owner->getComponent());
+    }
+    return false;
 }
 
 MultiTouchMapper<UITouch*> UIViewComponentPeer::currentTouches;
